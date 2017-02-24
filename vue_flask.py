524ee247285216a82ed7f70e03f7e7f77392701d
nu.everest <nu.everest@gmail.com>
2017-02-24 08:31:06 -0700

    Added static media url selection logic based on the server url.

--- vue_flask.py
+++ vue_flask.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+# Python
+from urlparse import urlparse
+
+# Flask
+from flask import Flask, render_template, request
 from flask import url_for as local_url_for
 from flask_s3 import FlaskS3
 from flask_s3 import url_for as s3_url_for
-
 
 class CustomFlask(Flask):
     """
@@ -31,9 +34,35 @@ s3 = FlaskS3(app)
 
 @app.route('/')
 def index():
-    favicon = s3_url_for('static', filename='favicon.ico')
-    blowdrycss = local_url_for('static', filename='css/blowdry.css')
-    return render_template('index.html', favicon=favicon, blowdrycss=blowdrycss)
+    favicon = select_url_for('static', filename='favicon.ico')
+    blowdrycss = select_url_for('static', filename='css/blowdry.min.css')
+    test = is_production()
+    return render_template('index.html', favicon=favicon, blowdrycss=blowdrycss, test=test)
+
+
+def is_production():
+    """ Determines if app is running on the production server or not.
+    Get Current URI.
+    Extract root location.
+    Compare root location against developer server value 127.0.0.1:5000.
+    :return: (bool) True if code is running on the production server, and False otherwise.
+    """
+    root_url = request.url_root
+    developer_url = 'http://127.0.0.1:5000/'
+    return root_url != developer_url
+
+
+def select_url_for(endpoint, filename):
+    """ Select the static media url based on the server url.
+
+    :param endpoint: (string) Media folder
+    :param filename: (string) Media filename including subpath if necessary e.g. 'css/main.css', 'flower.jpg'
+    :return: (string) Local url sub-path or full S3 url for media.
+    """
+    if is_production():
+        return s3_url_for(endpoint=endpoint, filename=filename)
+
+    return local_url_for(endpoint=endpoint, filename=filename)
 
 
 if __name__ == '__main__':
