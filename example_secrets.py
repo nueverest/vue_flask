a40b23df17db8177b7414f1b0068b7735813bd66
nu.everest <nu.everest@gmail.com>
2017-03-15 20:55:01 -0700

    Improved comments for how to create and use secrets.py.

--- example_secrets.py
+++ example_secrets.py
@@ -1,11 +1,18 @@
-import uuid
+    import uuid
 
 
-def is_production():
-    """ Determines if app is running on the production server or not.
+    def is_production():
+        """ Determines if app is running on the production server or not.
 
-    Compare uuid for the machine against the know uuid of the development machine.
-    :return: (bool) True if code is running on the production server, and False otherwise.
-    """
-    developer_machines = [111111111, ]
-    return uuid.getnode() not in developer_machines
+        HOW TO USE:
+        Open a terminal
+        > python
+        > import uuid
+        > uuid.getnode()
+        12345678987654321  <-- Copy whatever is returned and replace 111111111 with this.
+
+        Compare uuid for the machine against the known uuid(s) of your development machine(s).
+        :return: (bool) True if code is running on the production server, and False otherwise.
+        """
+        developer_machines = [111111111, ]
+        return uuid.getnode() not in developer_machines
