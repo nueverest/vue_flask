77d776ce5cc92b72e725c0578b86b3fd6e05ccb2
nu.everest <nu.everest@gmail.com>
2017-03-15 21:36:15 -0700

    Note to ignore and rename to secrets.py.

--- example_secrets.py
+++ example_secrets.py
@@ -11,6 +11,9 @@ def is_production():
     > uuid.getnode()
     12345678987654321  <-- Copy whatever is returned and replace 111111111 with this.
 
+    Ensure .gitignore excludes ``secrets.py``.
+    Finally, rename this file to ``secrets.py``.
+
     Compare uuid for the machine against the known uuid(s) of your development machine(s).
     :return: (bool) True if code is running on the production server, and False otherwise.
     """
